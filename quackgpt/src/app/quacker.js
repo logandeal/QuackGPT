4522b4292f4cdbc270f167377a3b9e6bcfe56b24
Logan Deal <53441579+logandeal@users.noreply.github.com>
2024-03-22 00:25:53 -0600

    add constructor naming fix

--- quackgpt/src/app/quacker.js
+++ quackgpt/src/app/quacker.js
@@ -1,5 +1,5 @@
-const gpt_tokenizer = require('gpt-tokenizer/cjs/model/gpt-3.5-turbo')
-const OpenAI = require('openai')
+const gpt_tokenizer = require("gpt-tokenizer/cjs/model/gpt-3.5-turbo");
+const OpenAI = require("openai");
 
 // const apiKey = process.env.GPT_API_KEY
 // const org = process.env.GPT_API_ORG
@@ -14,93 +14,114 @@ const OpenAI = require('openai')
 
 // models will be tried in sorted order
 const models = [
-    {
-        name: "gpt-3.5-turbo",
-        maxContext: 4096
-    },
-    {
-        name: "gpt-3.5-turbo-0125",
-        maxContext: 16385
-    }
-]
+  {
+    name: "gpt-3.5-turbo",
+    maxContext: 4096,
+  },
+  {
+    name: "gpt-3.5-turbo-0125",
+    maxContext: 16385,
+  },
+];
 
 class Quacker {
-    constructor(apiKey, organization, initialContext = "You are acting as a programmer's rubber duck. Programmers often talk to rubber ducks to work through problems. You will be asked programming questions, but you may also be chatted with in English. Please answer these questions as helpfully as possible, but also as briefly as possible. If you don't have enough information, ask for whatever you need.") {
-        this.messages = [{role: "system", content: initialContext}]
-        this.model = models[0]
-        this.openai = new OpenAI({
-            apiKey: apiKey,
-            organization: org
-        })
+  constructor(apiKey, org) {
+    this.messages = [
+      {
+        role: "system",
+        content:
+          "You are acting as a programmer's rubber duck. Programmers often talk to rubber ducks to work through problems. You will be asked programming questions, but you may also be chatted with in English. Please answer these questions as helpfully as possible, but also as briefly as possible. If you don't have enough information, ask for whatever you need.",
+      },
+    ];
+    this.model = models[0];
+    this.openai = new OpenAI({
+      apiKey: apiKey,
+      organization: org,
+    });
+  }
+
+  addMessage(newMessage) {
+    const newMessageObj = { role: "user", content: newMessage };
+    const newMessages = this.messages.concat([newMessageObj]);
+
+    for (const model of models) {
+      if (gpt_tokenizer.isWithinTokenLimit(newMessages, model.maxContext)) {
+        this.model = model;
+        this.messages.push(newMessageObj);
+        return true;
+      }
     }
 
-    addMessage(newMessage) {
-        const newMessageObj = {role: "user", content: newMessage}
-        const newMessages = this.messages.concat([newMessageObj])
+    return false;
+  }
 
-        for (const model of models) {
-            if (gpt_tokenizer.isWithinTokenLimit(newMessages, model.maxContext)) {
-                this.model = model
-                this.messages.push(newMessageObj)
-                return true
-            }
-        }
+  async summarizeMessage(
+    bigMessage,
+    targetLength = this.model.maxContext,
+    maxTries = 5
+  ) {
+    const summarizer = new Quacker(
+      "Rather than responding to my prompts, you need to summarize anything I say to you and tell me the summary. Do not say anything but the summary. Do not say 'The summary is' or anything like that, just say exactly the summary, no more and no less. If and only if the summary you want to give contains more tokens than the original response, try to think of another response. If you still can't think of a shorter response after 5 tries, say 'Error: the prompt cannot be summarized.' exactly like that."
+    );
 
-        return false
+    let workingSum = bigMessage;
+    for (let i = maxTries; i > 0; i--) {
+      summarizer.addMessage(workingSum);
+      workingSum = await summarizer.getFirstResponse();
+
+      if (workingSum == "Error: the prompt cannot be summarized.") {
+        console.log(workingSum);
+        break;
+      }
+
+      if (gpt_tokenizer.isWithinTokenLimit(workingSum, targetLength)) {
+        return workingSum;
+      }
     }
 
-    async summarizeMessage(bigMessage, targetLength = this.model.maxContext, maxTries = 5) {
-        const summarizer = new Quacker("Rather than responding to my prompts, you need to summarize anything I say to you and tell me the summary. Do not say anything but the summary. Do not say 'The summary is' or anything like that, just say exactly the summary, no more and no less. If and only if the summary you want to give contains more tokens than the original response, try to think of another response. If you still can't think of a shorter response after 5 tries, say 'Error: the prompt cannot be summarized.' exactly like that.")
+    throw new TooBigToSumError(bigMessage, maxTries);
+  }
 
-        let workingSum = bigMessage
-        for (let i = maxTries; i > 0; i--) {
-            summarizer.addMessage(workingSum)
-            workingSum = await summarizer.getFirstResponse()
+  async sendMessages() {
+    const completion = await openai.chat.completions.create({
+      messages: this.messages,
+      model: this.model.name,
+    });
 
-            if (workingSum == "Error: the prompt cannot be summarized.") {
-                console.log(workingSum)
-                break
-            }
+    return completion;
+  }
 
-            if (gpt_tokenizer.isWithinTokenLimit(workingSum, targetLength)) {
-                return workingSum
-            }
-        }
+  async getFirstResponse() {
+    const completion = await this.sendMessages();
 
-        throw new TooBigToSumError(bigMessage, maxTries)
-    }
+    return completion.choices[0].message.content;
+  }
 
-    async sendMessages() {
-        const completion = await openai.chat.completions.create({
-            messages: this.messages,
-            model: this.model.name,
-        });
+  async test() {
+    const summary = await this.summarizeMessage(
+      "Tell me how I might go about calculating 4 + 4 in code.",
+      50,
+      2
+    );
+    console.log(summary);
 
-        return completion
-    }
+    this.addMessage(summary);
 
-    async getFirstResponse() {
-        const completion = await this.sendMessages()
+    const response = await this.getFirstResponse();
 
-        return completion.choices[0].message.content
-    }
-
-    async test() {
-        const summary = await this.summarizeMessage("Tell me how I might go about calculating 4 + 4 in code.", 50, 2)
-        console.log(summary)
-
-        this.addMessage(summary)
-
-        const response = await this.getFirstResponse()
-
-        console.log(response);
-    }
+    console.log(response);
+  }
 }
 
 class TooBigToSumError extends Error {
-    constructor(bigMessage, steps) {
-        super("The following message was too big to summarize in " + steps + " steps: " + bigMessage)
-    }
+  constructor(bigMessage, steps) {
+    super(
+      "The following message was too big to summarize in " +
+        steps +
+        " steps: " +
+        bigMessage
+    );
+  }
 }
 
-module.exports = Quacker
+module.exports = Quacker;
