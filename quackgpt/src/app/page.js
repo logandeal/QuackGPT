ed4a0d8ea211347609e52afcd1b8b04dbb98b22b
Logan Deal <53441579+logandeal@users.noreply.github.com>
2024-04-02 16:17:55 -0500

    fix error this way for now

--- quackgpt/src/app/page.js
+++ quackgpt/src/app/page.js
@@ -5,20 +5,20 @@ import pkg from "../../package.json";
 
 export default function Landing() {
   // @ts-ignore
-  // if (window && !window.electronAPI) {
-  //   return (
-  //     <div>
-  //       <ul>
-  //         <li>
-  //           <a href={`/downloads/QuackGPT-${pkg.version}-arm64.dmg`}>Mac</a>
-  //         </li>
-  //         <li>
-  //           <a href={`/downloads/QuackGPT ${pkg.version}`}>Windows</a>
-  //         </li>
-  //       </ul>
-  //     </div>
-  //   );
-  // }
+  if (typeof window !== "undefined" && window && !window.electronAPI) {
+    return (
+      <div>
+        <ul>
+          <li>
+            <a href={`/downloads/QuackGPT-${pkg.version}-arm64.dmg`}>Mac</a>
+          </li>
+          <li>
+            <a href={`/downloads/QuackGPT ${pkg.version}`}>Windows</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
   return (
     <>
       <div>
